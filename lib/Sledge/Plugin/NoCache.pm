2c970170d9ee7b20196db23e697c5896b01e1b94
miyagawa <>
2003-02-14 07:42:25 +0000

    added POD

--- lib/Sledge/Plugin/NoCache.pm
+++ lib/Sledge/Plugin/NoCache.pm
@@ -31,3 +31,37 @@ sub set_no_cache {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+Sledge::Plugin::NoCache - plugin to add no-cache HTTP response
+
+=head1 SYNOPSIS
+
+  package Your::Pages;
+  use Sledge::Plugin::NoCache;
+
+  sub dispatch_foo {
+      my $self = shift;
+      $self->set_no_cache;
+  }
+
+  # always no-cache on POST request
+  use Sledge::Plugin::NoCache 'POST';
+
+=head1 DESCRIPTION
+
+C<no-cache> ヘッダを吐きだすメソッド C<set_no_cache> を提供するプラグインです。
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa <miyagawa@edge.co.jp> with Sledge development team.
+
+=head1 SEE ALSO
+
+HTTP/1.1
+
+=cut
+
